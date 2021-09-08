c53d5028e8db37065fd1da2a49cba0067aa87854
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-08 11:58:05 +0100

    #4: Populate app with segments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import './App.css';
 import Segment from './components/Segment';
 import Ticket, { TicketCardData } from './components/Ticket';
@@ -17,29 +17,41 @@ const ticket1 = {
 
 const tickets = Array<TicketCardData>(10).fill(ticket1);
 
+const styles: {[component: string]: CSSProperties} = {
+	app: { width: '100%', height: '100%' },
+	segmentsContainer: {
+		height: '100%',
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'stretch',
+		justifyContent: 'stretch',
+		gap: 10,
+		overflow: 'hidden',
+	},
+	segment: { width: 300 },
+};
+
 function App(): JSX.Element {
-	const appStyle = { width: '100%', height: '100%' };
 	return (
-		<div className="App" style={appStyle}>
-			<Ticket
-				style={{ width: 250 }}
-				data={{
-					id: 'TUN_75',
-					categories: [
-						{ name: 'feature', color: '#f00' },
-						{ name: 'b', color: '#0f0' },
-						{ name: 'c', color: '#00f' },
-					],
-					title: 'Fix the bug where the audio playback suddently stops',
-					urgency: 0.4,
-					assignees: [{ initials: 'RK', color: '#345' }, { initials: 'RK', color: '#f58' }],
-				}}
-			/>
-			<Segment
-				style={{ width: 300, height: 500 }}
-				data={{ name: 'In Progress', tickets }}
-			/>
-			Hello World!
+		<div className="App" style={styles.app}>
+			<div
+				className="segments-container"
+				style={styles.segmentsContainer}
+			>
+				<Segment
+					style={styles.segment}
+					data={{ name: 'In Progress', tickets }}
+				/>
+				<Segment
+					style={styles.segment}
+					data={{ name: 'In Progress', tickets }}
+				/>
+				<Segment
+					style={styles.segment}
+					data={{ name: 'In Progress', tickets }}
+				/>
+			</div>
 		</div>
 	);
 }
